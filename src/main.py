ba5f9e3cd51b2ef7c84402929701075000f37a66
yy-suisse <58217823+yy-suisse@users.noreply.github.com>
2020-11-22 19:55:35 +0100

    Add files via upload

--- src/main.py
+++ src/main.py
@@ -17,7 +17,7 @@ import start_thymio
 # -------------------------------------------------- VISION INIT --------------------------------------------------
 
 #video capture object
-cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+cap = cv2.VideoCapture(0)
 
 #read a few frames to allow the camera to adjust
 for i in range(20):
@@ -27,37 +27,55 @@ for i in range(20):
 #ret, frame = cap.read()
 
 frame = cv2.imread("vision/images/colors.png")
+<<<<<<< HEAD
+print(frame.shape[:2])
+=======
+#sizeframe
+>>>>>>> 49b56a36903e26308c5c50c900af746e44c7d156
 
 scale = vision.detect_scale(frame)
 
 obstacles, ret = vision.detect_obstacles(frame, scale)
-#print(obstacles)
+print(obstacles)
 
 targets, ret = vision.detect_targets(frame, scale)
 #print(targets)
 
 robot_pos, ret = vision.detect_robot(frame, scale)
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> 49b56a36903e26308c5c50c900af746e44c7d156
 # ------------------------------------------------- GLOBAL NAVIGATION --------------------------------------------
 
-dilatedObstacles = globalNavigation.dilateObstacles(obstacles, scalingFactor = 1.8) ########## scaling
+#dilatedObstacles = globalNavigation.dilateObstacles(obstacles, scalingFactor = 1.8) ########## scaling
 #print(dilatedObstacles)
 
-visibilityGraph, possibleDisplacement = globalNavigation.computeVisibilityGraph(dilatedObstacles)
+visibilityGraph, possibleDisplacement = globalNavigation.computeVisibilityGraph(obstacles)
 
 targets.insert(0, [robot_pos[0][0], robot_pos[0][1]]) # the initial position of the Thymio is the starting point of the trajectory
 
 trajectory = globalNavigation.computeTrajectory(visibilityGraph, targets)
 print("traject: ",trajectory)
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> 49b56a36903e26308c5c50c900af746e44c7d156
 final = vision.debug_output(frame, robot_pos, targets, obstacles, trajectory, scale)
 
 plt.figure()
 plt.imshow(cv2.cvtColor(final, cv2.COLOR_BGR2RGB))
 
+<<<<<<< HEAD
+=======
+
+
+>>>>>>> 49b56a36903e26308c5c50c900af746e44c7d156
 plt.figure()
 plt.gca().invert_yaxis()
-globalNavigation.printGlobalNavigation(obstacles, dilatedObstacles, interestPoints = targets, trajectory = trajectory)
+globalNavigation.printGlobalNavigation(obstacles, obstacles, interestPoints = targets, trajectory = trajectory)
 
 # ------------------------------------------------- CONNEXION --------------------------------------------------
 start_thymio.connexion_thymio()
@@ -66,9 +84,11 @@ value_proximity=[]
 value_acceleration=[]
 value_speed=[]
 actual_position=[0,0]
-goal=[0,0]
 actual_angle=0
-goal=trajectory[1]
+count_trajectory=1
+goal_actual=trajectory[count_trajectory]
+nb_goal=len(trajectory)
+
 # ------------------------------------------------- UPLOAD VARIABLE  --------------------------------------------------
 
 
@@ -88,6 +108,29 @@ while True:
     actual_position,actual_angle=start_thymio.get_position(cap) # upload data in global variables
     #print("actuel position est:",actual_position)
     #print("actuel angle est:",actual_angle)
-    start_thymio.follow_the_way_to_dream(actual_position,goal,actual_angle)
+    #start_thymio.follow_the_way_to_dream(actual_position,goal,actual_angle)
+    start_thymio.detect_obstacles()
+    if start_thymio.detect_trajectory(actual_position,goal_actual) and count_trajectory < nb_goal-1:         # upload goal 
+        count_trajectory+=1
+        goal_actual=trajectory[count_trajectory]
+        print("goal has just changed, actual goal is :",goal_actual)
+        #start_thymio.follow_the_way_to_dream(actual_position,goal_actual,actual_angle)
+        start_thymio.local_avoidance (actual_position,goal_actual,actual_angle)
+
+    elif start_thymio.detect_trajectory(actual_position,goal_actual) and count_trajectory == nb_goal-1:     # if all points are finished
+        start_thymio.mission_accomplished()
+
+    else:
+        start_thymio.local_avoidance (actual_position,goal_actual,actual_angle)
+        #start_thymio.follow_the_way_to_dream(actual_position,goal_actual,actual_angle)    
+       # print("actual pos: ",actual_position)    
+       # print("goal is :",goal_actual)  
+       # 
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+                       
+
 
 cap.release()
+cv2.destroyAllWindows()

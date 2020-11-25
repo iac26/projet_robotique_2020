7703a7b2b8bc276afc4a520e20234d9a44668452
timu1994 <63039935+timu1994@users.noreply.github.com>
2020-11-26 00:51:41 +0100

    Aues id class ta

--- src/filtrage/Extended_Kalman_Filter.py
+++ src/filtrage/Extended_Kalman_Filter.py
@@ -6,72 +6,6 @@ measurements = 0
 x = 0
 camera_avilable = 0
 
-def estimate(measurements):
-    # Time Update (Prediction)
-    # ========================
-    # Project the state ahead
-    # see "Dynamic Matrix"
-    if np.abs(measurements[4])<0.0001: # Driving straight
-        x[0] = x[0] + x[3]*dt * np.cos(x[2])
-        x[1] = x[1] + x[3]*dt * np.sin(x[2])
-        x[2] = x[2]
-        x[3] = x[3]
-        x[4] = 0.0000001 # avoid numerical issues in Jacobians
-    else: # otherwise
-        x[0] = x[0] + (x[3]/x[4]) * (np.sin(x[4]*dt+x[2]) - np.sin(x[2]))
-        x[1] = x[1] + (x[3]/x[4]) * (-np.cos(x[4]*dt+x[2])+ np.cos(x[2]))
-        x[2] = (x[2] + x[4]*dt + np.pi) % (2.0*np.pi) - np.pi
-        x[3] = x[3]
-        x[4] = x[4]
-    print('x =')
-    print(x)
-
-    # Calculate the Jacobian of the Dynamic Matrix A
-    # see "Calculate the Jacobian of the Dynamic Matrix with respect to the state vector"
-    a13 = float((x[3]/x[4]) * (np.cos(x[4]*dt+x[2]) - np.cos(x[2])))
-    a14 = float((1.0/x[4]) * (np.sin(x[4]*dt+x[2]) - np.sin(x[2])))
-    a15 = float((dt*x[3]/x[4])*np.cos(x[4]*dt+x[2]) - (x[3]/x[4]**2)*(np.sin(x[4]*dt+x[2]) - np.sin(x[2])))
-    a23 = float((x[3]/x[4]) * (np.sin(x[4]*dt+x[2]) - np.sin(x[2])))
-    a24 = float((1.0/x[4]) * (-np.cos(x[4]*dt+x[2]) + np.cos(x[2])))
-    a25 = float((dt*x[3]/x[4])*np.sin(x[4]*dt+x[2]) - (x[3]/x[4]**2)*(-np.cos(x[4]*dt+x[2]) + np.cos(x[2])))
-    JA = np.matrix([[1.0, 0.0, a13, a14, a15],
-                    [0.0, 1.0, a23, a24, a25],
-                    [0.0, 0.0, 1.0, 0.0, dt],
-                    [0.0, 0.0, 0.0, 1.0, 0.0],
-                    [0.0, 0.0, 0.0, 0.0, 1.0]])
-
-
-    # Project the error covariance ahead
-    P = JA*P*JA.T + Q
-
-    # Measurement Update (Correction)
-    # ===============================
-    # Measurement Function
-    hx = np.matrix([[float(x[0])],[float(x[1])],[float(x[2])],[float(x[3])],[float(x[4])]])
-    print('hx = ')
-    print(hx)
-
-    if camera_avilable: # with 10Hz, every 5th step
-        JH = np.diag([1.0, 1.0, 1.0, 1.0, 1.0])
-    else: # every other step
-        JH = np.diag([0.0, 0.0, 0.0, 1.0, 1.0])  
-
-    # Calculating Kalman gain     
-    S = JH*P*JH.T + R
-    K = (P*JH.T) * np.linalg.inv(S)
-
-    # Update the estimate via
-    Z = measurements.reshape(JH.shape[0],1)
-    y = Z - (JH*hx)                         # I added JH .... not 100% sure
-    x = x + (K*y)
-
-    # Update the error covariance
-    P = (I - (K*JH))*P
-
-
-init()
-estimate(measurements)
-
 
 class Kalman():
 
@@ -110,7 +44,7 @@ class Kalman():
         print(R, R.shape)
 
         # Identity Matrix I
-        self.I = np.eye(numstates)
+        self.I = np.eye(self.numstates)
         print('I =')
         print(I, I.shape)
 
@@ -128,11 +62,67 @@ class Kalman():
         print('measurements = ')
         print(measurements, measurements.shape)
 
-        
-
     def estimate(self, measurements):
-        print(self.numstates)
+        # Time Update (Prediction)
+        # ========================
+        # Project the state ahead
+        # see "Dynamic Matrix"
+        if np.abs(measurements[4])<0.0001: # Driving straight
+            x[0] = x[0] + x[3]*dt * np.cos(x[2])
+            x[1] = x[1] + x[3]*dt * np.sin(x[2])
+            x[2] = x[2]
+            x[3] = x[3]
+            x[4] = 0.0000001 # avoid numerical issues in Jacobians
+        else: # otherwise
+            x[0] = x[0] + (x[3]/x[4]) * (np.sin(x[4]*dt+x[2]) - np.sin(x[2]))
+            x[1] = x[1] + (x[3]/x[4]) * (-np.cos(x[4]*dt+x[2])+ np.cos(x[2]))
+            x[2] = (x[2] + x[4]*dt + np.pi) % (2.0*np.pi) - np.pi
+            x[3] = x[3]
+            x[4] = x[4]
+        print('x =')
+        print(x)
 
+        # Calculate the Jacobian of the Dynamic Matrix A
+        # see "Calculate the Jacobian of the Dynamic Matrix with respect to the state vector"
+        a13 = float((x[3]/x[4]) * (np.cos(x[4]*dt+x[2]) - np.cos(x[2])))
+        a14 = float((1.0/x[4]) * (np.sin(x[4]*dt+x[2]) - np.sin(x[2])))
+        a15 = float((dt*x[3]/x[4])*np.cos(x[4]*dt+x[2]) - (x[3]/x[4]**2)*(np.sin(x[4]*dt+x[2]) - np.sin(x[2])))
+        a23 = float((x[3]/x[4]) * (np.sin(x[4]*dt+x[2]) - np.sin(x[2])))
+        a24 = float((1.0/x[4]) * (-np.cos(x[4]*dt+x[2]) + np.cos(x[2])))
+        a25 = float((dt*x[3]/x[4])*np.sin(x[4]*dt+x[2]) - (x[3]/x[4]**2)*(-np.cos(x[4]*dt+x[2]) + np.cos(x[2])))
+        JA = np.matrix([[1.0, 0.0, a13, a14, a15],
+                        [0.0, 1.0, a23, a24, a25],
+                        [0.0, 0.0, 1.0, 0.0, dt],
+                        [0.0, 0.0, 0.0, 1.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 1.0]])
+
+
+        # Project the error covariance ahead
+        P = JA*P*JA.T + Q
+
+        # Measurement Update (Correction)
+        # ===============================
+        # Measurement Function
+        hx = np.matrix([[float(x[0])],[float(x[1])],[float(x[2])],[float(x[3])],[float(x[4])]])
+        print('hx = ')
+        print(hx)
+
+        if camera_avilable: # with 10Hz, every 5th step
+            JH = np.diag([1.0, 1.0, 1.0, 1.0, 1.0])
+        else: # every other step
+            JH = np.diag([0.0, 0.0, 0.0, 1.0, 1.0])  
+
+        # Calculating Kalman gain     
+        S = JH*P*JH.T + R
+        K = (P*JH.T) * np.linalg.inv(S)
+
+        # Update the estimate via
+        Z = measurements.reshape(JH.shape[0],1)
+        y = Z - (JH*hx)                         # I added JH .... not 100% sure
+        x = x + (K*y)
+
+        # Update the error covariance
+        P = (I - (K*JH))*P
 
     def update_measurements(self, robot_state, thymio_data):
         robot_x = robot_state[0][0]
@@ -142,9 +132,6 @@ class Kalman():
         yawrate = thymio_data[1]
         self.camera_avilable = robot_state[2]
         self.measurements = [robot_x, robot_y, robot_phi, speed, yawrate]
-
-        
-
 
     def get_result(self):
         return self.result

699673aff39d7c3f744fd841e5a1addcd6d984f5
yuan <zyy.suisse@gmail.com>
2020-12-01 16:22:36 +0100

    lol

--- src/localNavigation/start_thymio.py
+++ src/localNavigation/start_thymio.py
@@ -13,21 +13,24 @@ from Thymio import Thymio
 sys.path.append("vision")
 import vision
 ############# CONSTANTES ######################
-TOLERENCE_POSITION = 2
+TOLERENCE_POSITION = 10
 ERROR_TOLERENCE=0.1
 THRESHOLD_DIST = 2000
 
 ############# GLOBAL VARIABLES ######################
-obstacles_bool = 0
+counter = 0
 value_proximity=[0,0,0,0,0,0,0]
 value_acceleration=[0,0]
 value_speed=[0,0]
 actual_position=[0,0]
 actual_angle=0
 actual_goal=[0,0]
-
-error=0
+no_detection=False
 error_sum=0
+error = 0
+error_prev=0
+speed_avoidance_l_prev=0
+speed_avoidance_r_prev=0
 
 
 
@@ -62,7 +65,7 @@ class RepeatedTimer(object):
 
 def connexion_thymio():
     global th
-    th = Thymio.serial(port="COM8", refreshing_rate=0.1)
+    th = Thymio.serial(port="COM5", refreshing_rate=0.1)
     time.sleep(3) # To make sure the Thymio has had time to connect
     print("Thymio is connected :)")
 
@@ -89,25 +92,13 @@ def get_sensor_value():
 
 
 
-def get_position(frame):
+def get_position(robot_pos):
     global actual_angle
     global actual_position
-    global robot_pos
-    #get one frame
-    
-    scale = vision.detect_scale(frame)
-    robot_pos, ret = vision.detect_robot(frame, scale)
-    #get one frame
-
-    obstacles, ret = vision.detect_obstacles(frame, scale)
-    #print(obstacles)
-
-    targets, ret = vision.detect_targets(frame, scale)
-    #print(targets)
-    #print(robot_pos)
-    #print("robot position:",robot_pos[0])
-    #print("robot angle:",robot_pos[1])
+    global no_detection
+  
     if robot_pos[2]==True:
+        no_detection= False
         #print("robot position:",robot_pos[0])
         #print("robot angle:",robot_pos)
         actual_position= [robot_pos[0][0],robot_pos[0][1]] 
@@ -115,35 +106,53 @@ def get_position(frame):
         actual_angle=robot_pos[1]
         return actual_position,actual_angle
     else: 
+        no_detection=True
         return actual_position,actual_angle
-    
-"""
-def compute_angle_goal (actual_position,goal,actual_angle):
-    return math.atan((goal[1]-actual_position[1])/(goal[0]-actual_position[0]))-actual_angle
-"""
 
-def compute_angle_goal (actual_position,goal):
-    return math.atan((goal[1]-actual_position[1])/(goal[0]-actual_position[0]))
 
-def calculate_error(goal):
+def calculate_error(actual_position,goal,actual_angle):
     global error_sum 
+    global no_detection
     global error
+    global error_prev
+    goal_array = np.array([goal[0],goal[1]])
+    actual_position_array = np.array([actual_position[0],actual_position[1]])
+   
+    direction = goal_array - actual_position_array
+    angle = np.arctan2(direction[1], direction[0])
+    error_prev = error
+    error = -actual_angle + angle 
 
-    angle_b=compute_angle_goal(actual_position,goal)
-    delta_x=goal[0]-actual_position[0]
-    delta_y=goal[1]-actual_position[1]
-    if delta_x >= 0 and delta_y <= 0:
-        error=angle_b - actual_angle
-    elif delta_x <= 0 and delta_y <= 0:
-        error=-1*math.pi + angle_b - actual_angle
-    elif delta_x < 0 and delta_y > 0:
-        error=(math.pi) + angle_b - actual_angle 
-    elif delta_x > 0 and delta_y > 0:
-        error = angle_b - actual_angle 
+    if error < -np.pi:
+        error += 2*np.pi 
+    if error >  np.pi:
+        error -= 2*np.pi 
 
-    error_sum = error_sum + error
+    error_sum += error
+
+    return error
 
 
+
+""" 
+   if no_detection==False:
+        angle_b=compute_angle_goal(actual_position,goal)
+        delta_x=goal[0]-actual_position[0]
+        delta_y=goal[1]-actual_position[1]
+        if delta_x >= 0 and delta_y <= 0:
+            error=angle_b - actual_angle
+        elif delta_x <= 0 and delta_y <= 0:
+            error=-1*math.pi + angle_b - actual_angle
+        elif delta_x < 0 and delta_y > 0:
+            error=(math.pi) + angle_b - actual_angle 
+        elif delta_x > 0 and delta_y > 0:
+            error = angle_b - actual_angle 
+        error_sum = error_sum + error
+        print("actual pos: ",actual_position)
+        print("actual goal: ",goal)
+    else:
+        error=0
+"""
 
 
 def follow_the_way_to_dream(actual_position,goal,actual_angle):
@@ -151,52 +160,98 @@ def follow_the_way_to_dream(actual_position,goal,actual_angle):
     base_speed, kp,ki à tunner
     """
     global error_sum 
-    global error
-    base_speed = 25
-    kp = 200
-    ki = 5
-    saturation = 500
-    error_sat=500/ki
-    angle_b=compute_angle_goal(actual_position,goal)
-    delta_x=goal[0]-actual_position[0]
-    delta_y=goal[1]-actual_position[1]
+    global value_proximity
+    global counter
+    global speed_avoidance_l_prev
+    global speed_avoidance_r_prev
 
-    if delta_x >= 0 and delta_y <= 0:
-        error=angle_b - actual_angle
-    elif delta_x <= 0 and delta_y <= 0:
-        error=-1*math.pi + angle_b - actual_angle
-    elif delta_x < 0 and delta_y > 0:
-        error=(math.pi) + angle_b - actual_angle 
-    elif delta_x > 0 and delta_y > 0:
-        error = angle_b - actual_angle 
+    if no_detection==False:
+        x=np.array([0,0,0,0,0,0,0,0,0])
+        sensor_scale = 1500
+        w_l = np.array([40,  20, -20, -20, -40,  30, -10, 8, 0])
+        w_r = np.array([-40, -20, 20,  20,  40, -10,  30, 0, 8])
+        x[:7]= np.array(value_proximity) / sensor_scale
+        x[7] = speed_avoidance_l_prev / 10 #10
+        x[8] = speed_avoidance_r_prev / 10 #10
+
+        speed_avoidance_l = np.sum(x * w_l)
+        speed_avoidance_r = np.sum(x * w_r)
+        speed_avoidance_l_prev=speed_avoidance_l
+        speed_avoidance_r_prev=speed_avoidance_r
+
+        if x[7] != 0 or x[8] != 0:
+            base_speed = 150
+        else : 
+            base_speed = 75 
 
 
-    error_sum = error_sum + error
+        error = calculate_error(actual_position,goal,actual_angle)
+        print("error: ",error)
+        """
+        if any(np.array(value_proximity) > 3000) :
+            #counter=20
 
-    if error_sum>error_sat:
-        error_sum=error_sat
-    if error_sum<-error_sat:
-        error_sum=-error_sat
+        if counter > 0:
+            counter -= 1
+            print("counter:",counter)
+            kp = 5
+            ki = 0
+            base_speed = 100 #75
 
-    speed_l= base_speed + (kp*error+ki*error_sum)
-    speed_r= base_speed - (kp*error+ki*error_sum)
+
+        else:
+            kp = 100
+            ki = 5
+            base_speed = 50
+        """
+
+        
+        kp = 100
+        ki = 3.5
+        kd = 8
  
+        error_sat = 10
 
-    if speed_l>saturation:
-        speed_l=saturation
-    if speed_l<-saturation:
-        speed_l=-saturation
-    if speed_r>saturation:
-        speed_r=saturation
-    if speed_r<-saturation:
-        speed_r=-saturation
+        if error_sum>error_sat:
+            error_sum=error_sat
+        if error_sum<-error_sat:
+            error_sum=-error_sat
+        
+        print("error_sum: ",error_sum)
 
-    #print("total error: ",error_sum)    
-    #print("error: ",error)    
-    #print("speed left: ",speed_l)    
-    #print("speed right: ",speed_r) 
-    #move(int(speed_l),int(speed_r))
-    return int(speed_l),int(speed_r)
+        vitesse_PID = kp*error+ki*error_sum + kd *(error-error_prev)
+
+        speed_l = base_speed + vitesse_PID + speed_avoidance_l
+        speed_r = base_speed - vitesse_PID + speed_avoidance_r
+        
+        move(int(speed_l),int(speed_r))
+
+        """
+        if obstacles_bool:
+            direction = value_proximity.index(max(value_proximity[0:5]))
+            if direction == 0 or direction == 1:
+                speed_l_a= speed_a
+                speed_r_a= -speed_a
+            if direction == 3 or direction == 4:
+                speed_l_a= -speed_a
+                speed_r_a= speed_a
+            if direction == 2 :
+                speed_l_a= -speed_a
+                speed_r_a= speed_a
+            speed_final_l=w_obstacle[0]*speed_l+w_obstacle[1]*speed_l_a
+            speed_final_r=w_obstacle[0]*speed_r+w_obstacle[1]*speed_r_a
+            print("error: ", error)
+            #move(0,0)
+            move(int(speed_final_l),int(speed_final_r))
+            
+        else : 
+            print("error: ", error)
+            #move(0,0)
+            move(int(speed_l),int(speed_r))
+        """    
+    else:
+        stop()
+ 
     
 
 def move(l_speed=500, r_speed=500, verbose=False):
@@ -217,6 +272,7 @@ def move(l_speed=500, r_speed=500, verbose=False):
     th.set_var("motor.left.target", l_speed)
     th.set_var("motor.right.target", r_speed)
 
+
 def stop(verbose=False):
     """
     param verbose: whether to print status messages or not
@@ -229,50 +285,50 @@ def stop(verbose=False):
     th.set_var("motor.left.target", 0)
     th.set_var("motor.right.target", 0)
 
+
 def detect_trajectory(actual_position,goal_actual): 
     if (abs(goal_actual[0]-actual_position[0])<=TOLERENCE_POSITION) and (abs(goal_actual[1]-actual_position[1])<=TOLERENCE_POSITION):   
         return True
     else:
         return False
 
+
 def mission_accomplished():
     stop()
     print("Mission is accomplished!! ")
 
 
-
-
-
-
+"""
 def local_avoidance (actual_position,goal,actual_angle):
-    w_obstacle=[0.1,0.9]
-    speed_base=30
-    speed_a=100
-    speed_l, speed_r = follow_the_way_to_dream(actual_position,goal,actual_angle)
-    if obstacles_bool:
-        direction = value_proximity.index(max(value_proximity[0:5]))
-        if direction == 0 or direction == 1:
-            speed_l_a= speed_a
-            speed_r_a= -speed_a
-        if direction == 3 or direction == 4:
-            speed_l_a= -speed_a
-            speed_r_a= speed_a
-        if direction == 2 :
-            speed_l_a= -speed_a
-            speed_r_a= speed_a
-        speed_final_l=speed_base+w_obstacle[0]*speed_l+w_obstacle[1]*speed_l_a
-        speed_final_r=speed_base+w_obstacle[0]*speed_r+w_obstacle[1]*speed_r_a
-        move(int(speed_final_l),int(speed_final_r))
-
+    if no_detection==False:
+        calculate_error(actual_position,goal,actual_angle)
+        w_obstacle=[0.1,0.9]
+        speed_base=30
+        speed_a=100
+        speed_l, speed_r = follow_the_way_to_dream(actual_position,goal,actual_angle)
+        if obstacles_bool:
+            direction = value_proximity.index(max(value_proximity[0:5]))
+            if direction == 0 or direction == 1:
+                speed_l_a= speed_a
+                speed_r_a= -speed_a
+            if direction == 3 or direction == 4:
+                speed_l_a= -speed_a
+                speed_r_a= speed_a
+            if direction == 2 :
+                speed_l_a= -speed_a
+                speed_r_a= speed_a
+            speed_final_l=w_obstacle[0]*speed_l+w_obstacle[1]*speed_l_a
+            speed_final_r=w_obstacle[0]*speed_r+w_obstacle[1]*speed_r_a
+            print("error: ", error)
+            #move(0,0)
+            move(int(speed_final_l),int(speed_final_r))
             
-        
-    else : 
-        speed_l_a= 0
-        speed_r_a= 0
-        speed_final_l=speed_l
-        speed_final_r=speed_r
-        move(int(speed_final_l),int(speed_final_r))
-
+        else : 
+            print("error: ", error)
+            #move(0,0)
+            move(int(speed_l),int(speed_r))
+    else:
+        stop()
 
 
 
@@ -282,8 +338,6 @@ def detect_obstacles (): # upload if obstacle is present
     for x in value_proximity[0:5]:  #front horiton prox
         if x>=THRESHOLD_DIST:     # 3000 --> 7cm white surface
             obstacles_bool = 1
-
-    #print(obstacles_bool)
 
 
 def avoidance_move():
@@ -297,49 +351,5 @@ def avoidance_move():
     speed_l= base_speed + np.sum(x * w_l)
     speed_r= base_speed + np.sum(x * w_r)
     move(int(speed_l),int(speed_r))
+"""
 
-
-def normal_move():
-    global error_sum
-    global error
-    base_speed = 25
-    kp = 200
-    ki = 5
-    saturation = 500
-    error_sat=500/ki
-
-    if error_sum>error_sat:
-        error_sum=error_sat
-    if error_sum<-error_sat:
-        error_sum=-error_sat
-
-    speed_l= base_speed + (kp*error+ki*error_sum)
-    speed_r= base_speed - (kp*error+ki*error_sum)
- 
-
-    if speed_l>saturation:
-        speed_l=saturation
-    if speed_l<-saturation:
-        speed_l=-saturation
-    if speed_r>saturation:
-        speed_r=saturation
-    if speed_r<-saturation:
-        speed_r=-saturation
-
-    #print("total error: ",error_sum)    
-    #print("error: ",error)    
-    #print("speed left: ",speed_l)    
-    #print("speed right: ",speed_r) 
-    move(int(speed_l),int(speed_r))
-    #return int(speed_l),int(speed_r)
-
-
-def change_mode(goal):
-    detect_obstacles ()
-    calculate_error(goal)
-
-    if obstacles_bool==1:
-        avoidance_move()
-
-    if error<ERROR_TOLERENCE and obstacles_bool==0:
-        normal_move()

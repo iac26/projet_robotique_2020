ec7df3801e32646851ab9177e9e79389cc3b5ab9
yuan <zyy.suisse@gmail.com>
2020-12-06 18:20:50 +0100

    HIHI

--- src/localNavigation/start_thymio.py
+++ src/localNavigation/start_thymio.py
@@ -46,37 +46,6 @@ speed_avoidance_r_prev = 0
 
 ############# FUNCTION DEFINITIONS ######################
 
-class RepeatedTimer(object):
-    """
-    definition of timer, which can be called in a fixed interval
-    """
-    def __init__(self, interval, function, *args, **kwargs):
-        self._timer     = None
-        self.interval   = interval
-        self.function   = function
-        self.args       = args
-        self.kwargs     = kwargs
-        self.is_running = False
-        self.start()
-
-    def _run(self):
-        self.is_running = False
-        self.start()
-        self.function(*self.args, **self.kwargs)
-
-    def start(self):
-        if not self.is_running:
-            self._timer = Timer(self.interval, self._run)
-            self._timer.start()
-            self.is_running = True
-
-    def stop(self):
-        self._timer.cancel()
-        self.is_running = False
-
-
-
-
 def connexion_thymio():
     """
     This function should be called in order to connect Thymio
